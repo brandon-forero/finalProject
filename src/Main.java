28e9d595232784f0b5c2f5c102ecc62d44cdd74c
brandon.forero <brandon.forero@globant.com>
2025-02-23 08:43:41 -0500

    -Add initialisation function for basic initial university data -Add list classes menu option functionality - Add new class functionality

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,7 @@ public class Main {
         int option;
         int teacherType;
         University university = new University();
+        initUniversity(university);
 
         do {
             System.out.println("\nWelcome to the university system\nSelect an option:");
@@ -27,10 +30,42 @@ public class Main {
                         Teacher teacher = university.getTeachers().get(i);
                         System.out.println(i + ". \n name: " + teacher.getName() + ", salary: $" + teacher.getSalary());
                     }
+                    pressEnterToContinue();
                     break;
                 case 2:
-                    System.out.println("selected: 2");
-                    break;
+                    System.out.println("Classes: ");
+                    for(int i = 0; i < university.getClasses().size(); i++){
+                        Class currentClass = university.getClasses().get(i);
+                        System.out.println(i + ". " + currentClass.getName());
+                    }
+                    System.out.println(university.getClasses().size() + ". Return to menu");
+                    System.out.println("Select a class for see more info ");
+                    int selectedClass = scan.nextInt();
+
+                    if(selectedClass < university.getClasses().size()){
+                        System.out.println("Class information: ");
+                        System.out.println("Name: " + university.getClasses().get(selectedClass).getName());
+                        System.out.println("Classroom: " + university.getClasses().get(selectedClass).getClassRoom());
+                        System.out.println("Teacher: " + university.getClasses().get(selectedClass).getTeacher().getName());
+                        System.out.println("Students: ");
+                        for(Student student : university.getClasses().get(selectedClass).getStudents()){
+                            System.out.println(student.getId() + ". " + student.getName());
+                        }
+                        pressEnterToContinue();
+                        break;
+                    }
+                    else if(selectedClass == university.getClasses().size()){
+                        System.out.println("Returning to menu.. ");
+                        pressEnterToContinue();
+                        break;
+                    }
+
+                    else {
+                        System.out.println("Wrong option :(, returning to menu.. ");
+                        pressEnterToContinue();
+                        break;
+                    }
+
                 case 3:
                     System.out.println("Creating a new student...");
                     System.out.println("Student name: ");
@@ -48,7 +83,63 @@ public class Main {
                     pressEnterToContinue();
                     break;
                 case 4:
-                    System.out.println("selected: 4");
+                    System.out.println("Class name: ");
+                    scan.nextLine();
+                    String newClassName = scan.nextLine();
+                    System.out.println("Classroom name: ");
+                    String newClassroom = scan.nextLine();
+                    System.out.println("Select teacher for the class: ");
+                    for(int i = 0; i < university.getTeachers().size(); i++){
+                        Teacher teacher = university.getTeachers().get(i);
+                        System.out.println(i + ". " + teacher.getName());
+                    }
+                    int classTeacher = scan.nextInt();
+                    if (classTeacher < 0 || classTeacher > university.getTeachers().size()){
+                        System.out.println("invalid option :(, try again");
+                    }else{
+                        Teacher newClassTeacher = university.getTeachers().get(classTeacher);
+                        Class newClass = new Class(newClassName, newClassroom, newClassTeacher);
+                        ArrayList<Student> allStudents = new ArrayList<>(university.getStudents());
+                        int selectedStudent = 0;
+
+                        while (selectedStudent != university.getStudents().size()){
+                            System.out.println("Select an student for the class");
+                            allStudents.removeAll(newClass.getStudents());
+                            for(Student student : allStudents) {
+                                System.out.println(student.getId() + ". " + student.getName());
+                            }
+                            System.out.println(university.getStudents().size() + ". Finish register");
+
+                            selectedStudent = scan.nextInt();
+                            if(selectedStudent >= 0 && selectedStudent < university.getStudents().size()){
+                                if (allStudents.contains(university.getStudents().get(selectedStudent)) && !newClass.getStudents().contains(university.getStudents().get(selectedStudent))){
+                                    newClass.addStudent(university.getStudents().get(selectedStudent));
+                                    System.out.println("Student " + university.getStudents().get(selectedStudent).getName() + " Added to the " + newClass.getName() + " class" );
+                                    pressEnterToContinue();
+                                }else{
+                                    System.out.println("Invalid option :(, try again");
+                                    pressEnterToContinue();
+                                }
+
+                            }else if (selectedStudent == university.getStudents().size()){
+                                System.out.println("Returning to menu...");
+                                pressEnterToContinue();
+                            }
+                            else {
+                                System.out.println("Invalid option :(, try again");
+
+                            }
+
+                        }
+
+                        university.addClass(newClass);
+                        System.out.println("Class with the next info was successfully created: ");
+                        System.out.println("Class name: " + newClass.getName());
+                        System.out.println("Classroom: " + newClass.getClassRoom());
+                        System.out.println("Class Teacher: " + newClass.getTeacher().getName());
+                    }
+
+
                     break;
                 case 5:
                     System.out.println("Creating a new teacher...");
@@ -106,5 +197,50 @@ public class Main {
         System.out.println("Press enter to continue...");
         scan.nextLine();
         scan.nextLine();
+    }
+
+    public static void initUniversity(University university) {
+        String[] studentsNames = {"Brandon", "Maicol", "David", "Gabrielle", "Copito", "Sebastian"};
+        int exampleAge = 18;
+        for(String name : studentsNames){
+            Student newStudent = new Student(university.getStudents().size(), name, exampleAge);
+            university.addStudent(newStudent);
+            exampleAge++;
+        }
+
+        String[] teachersNames = {"Alice", "Bob", "Charlie", "Diana"};
+        int exampleHours = 10;
+        int exampleYears = 2;
+        for(int i = 0; i < teachersNames.length; i++){
+            if(i < 2){
+                PartTimeTeacher newTeacher = new PartTimeTeacher(teachersNames[i], 10000, exampleHours);
+                university.addTeacher(newTeacher);
+                exampleHours++;
+            }else{
+                FullTimeTeacher newTeacher = new FullTimeTeacher(teachersNames[i], 10000, exampleYears);
+                university.addTeacher(newTeacher);
+                exampleYears++;
+            }
+
+        }
+
+        String[] classes = {"Spanish", "Mathematics", "Physics", "Music"};
+        String[] classrooms = {"401", "402"};
+        for(int i = 0; i < classes.length; i++){
+            if(i < 2){
+                Class newClass = new Class(classes[i], classrooms[0], university.getTeachers().get(i));
+                newClass.addStudent(university.getStudents().get(i));
+                newClass.addStudent(university.getStudents().get(4));
+                university.addClass(newClass);
+            }else{
+                Class newClass = new Class(classes[i], classrooms[1], university.getTeachers().get(i));
+                newClass.addStudent(university.getStudents().get(i));
+                newClass.addStudent(university.getStudents().get(5));
+                university.addClass(newClass);
+            }
+
+        }
+
+
     }
 }
